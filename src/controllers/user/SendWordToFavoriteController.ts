3e5208e1f77d28cbf2e8f14f41ca35ba10e9c4b1
zeca.rt22 <zeca.rt22@gmail.com>
2022-10-05 13:42:36 -0300

    feat/send word to favorite route done

---
+++ src/controllers/user/SendWordToFavoriteController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { SendWordToFavoriteService } from "../../services/word/SendWordToFavoriteService";
+
+class SendWordToFavoriteController {
+  async handle(req: Request, res: Response) {
+    const word_id = req.query.word_id as string;
+
+    const sendWordToFavoriteService = new SendWordToFavoriteService();
+
+    const word = await sendWordToFavoriteService.execute({
+      word_id,
+    });
+
+    return res.json(word);
+  }
+}
+
+export { SendWordToFavoriteController };
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAthenticated } from "./middlewares/isAthenticated";
 import { ListWordsGenerateController } from "./controllers/word/ListWordsController";
+import { SendWordToFavoriteController } from "./controllers/user/SendWordToFavoriteController";
 const router = Router();
 
 //-- ROTAS USER --
@@ -17,5 +18,11 @@ router.get("/user/details", isAthenticated, new DetailUserController().handle);
 /* ROTAS WORD */
 
 router.post("/words", isAthenticated, new ListWordsGenerateController().handle);
+
+router.put(
+  "/word/favorite",
+  isAthenticated,
+  new SendWordToFavoriteController().handle
+);
 
 export { router };
